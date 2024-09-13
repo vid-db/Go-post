20d6a1b9d789ebe22eff6907d99d2307660d7e6d
David <daviddistrajobautista@gmail.com>
2024-09-14 02:33:18 +0800

    loading fallback

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,5 +1,6 @@
 import PostList from "@/components/postList";
 import { Suspense } from "react";
+import Loading from "./[id]/loading";
 
 const AllPosts = async () => {
     return (
@@ -7,7 +8,7 @@ const AllPosts = async () => {
             <h1 className="text-4xl font-bold md:text-5xl mb-10">
                 Random posts
             </h1>
-            <Suspense fallback="ssdfsdf">
+            <Suspense fallback={<Loading />}>
                 <PostList />
             </Suspense>
         </main>
