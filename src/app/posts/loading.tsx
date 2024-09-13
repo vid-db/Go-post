0763c0caee7584fe2146b9074077fed0ffd5f892
David <daviddistrajobautista@gmail.com>
2024-09-14 02:52:06 +0800

    loading fallback

--- src/app/posts/loading.tsx
+++ src/app/posts/loading.tsx
@@ -1,14 +1,28 @@
-import Ldr from "@/components/ldr"
+import Ldr from "@/components/ldr";
+
+import { useEffect, useState } from 'react';
 
 const Loading = () => {
+    const [isClient, setIsClient] = useState(false);
+
+    useEffect(() => {
+        setIsClient(true);
+    }, []);
+
     return (
         <div className="mx-auto my-auto text-xl">
-            <div>
-                <Ldr />
-            </div>
-            <p className="mt-3">Loading...</p>
+            {isClient ? (
+                <>
+                    <div>
+                        <Ldr />
+                    </div>
+                    <p className="mt-3">Loading...</p>
+                </>
+            ) : (
+                <p>Loading...</p>
+            )}
         </div>
-    )
-}
+    );
+};
 
-export default Loading
+export default Loading;
