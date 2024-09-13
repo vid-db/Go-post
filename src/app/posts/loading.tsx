b1d0369558dc564201731b188882d5c6cb6448b3
David <daviddistrajobautista@gmail.com>
2024-09-14 02:58:34 +0800

    loading fallback

--- src/app/posts/loading.tsx
+++ src/app/posts/loading.tsx
@@ -1,28 +1,14 @@
-import Ldr from "@/components/ldr";
-
-import { useEffect, useState } from 'react';
+import Ldr from "@/components/ldr"
 
 const Loading = () => {
-    const [isClient, setIsClient] = useState(false);
-
-    useEffect(() => {
-        setIsClient(true);
-    }, []);
-
     return (
         <div className="mx-auto my-auto text-xl">
-            {isClient ? (
-                <>
-                    <div>
-                        <Ldr />
-                    </div>
-                    <p className="mt-3">Loading...</p>
-                </>
-            ) : (
-                <p>Loading...</p>
-            )}
+            <div>
+                <Ldr />
+            </div>
+            <p className="mt-3">Loading...</p>
         </div>
-    );
-};
+    )
+}
 
-export default Loading;
+export default Loading
--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,13 +1,14 @@
 import PostList from "@/components/postList";
 
-
 const AllPosts = async () => {
     return (
         <main className="text-center p-16 px-5">
             <h1 className="text-4xl font-bold md:text-5xl mb-10">
                 Random posts
             </h1>
+
             <PostList />
+
         </main>
     );
 }
